2049eb9d4895a799cd19c7950450ac72d704035c
Isaac <ryanwoo1989@gmail.com>
2015-03-09 00:53:20 -0400

    Create ExprEval.h

---
+++ Expression-Evaluation/ExprEval.h
@@ -0,0 +1,31 @@
+#ifndef EXPREVAL_H_INCLUDED
+#define EXPREVAL_H_INCLUDED
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class ExprEval{
+public:
+    ExprEval():i(0){}
+    ExprEval(const string &ss):s(ss),i(0){}
+    void setName(const string &,double);
+    double getName(const string &) const;
+    void printNames() const;
+    void setExpr(const string &);
+    string getExpr()    const;
+    void clearTable();
+    double eval();
+private:
+    double number();
+    double prim();
+    double term();
+    double expr();
+    int i;
+    string s;
+    unordered_map<string,double> table;
+};
+
+
+#endif // EXPREVAL_H_INCLUDED
